49092a3147843f1c4008b17432246c2d380c877a
Peter Pathirana <peter@latent.io>
2024-04-27 20:56:35 +0000

    test(k3s): timeout fix

--- k3s/roles/install_k3s_service/files/k3s-node-init.sh
+++ k3s/roles/install_k3s_service/files/k3s-node-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 SERVER_URL=""
 NODE_TYPE="agent"
@@ -21,41 +21,48 @@ while [[ $# -gt 0 ]]; do
   shift
 done
 
-# setting needed environment variables
-export INSTALL_K3S_SKIP_DOWNLOAD=true
-if [[ "$SERVER_URL" != "" ]]; then
-  export K3S_URL="$SERVER_URL"
-fi
-if [[ "$NODE_TYPE" == "server" ]]; then
-  # on server nodes, set both 'server-token' and 'agent-token'
-  export K3S_TOKEN_FILE=/tmp/k3s_token_server
-  export K3S_AGENT_TOKEN_FILE=/tmp/k3s_token_agent
-else
-  # on agent nodes, only 'agent-token' needs to be set
-  export K3S_TOKEN_FILE=/tmp/k3s_token_agent
-fi
+determine_install_params() {
+  local node_type="${1:?}"
+  local server_url="$2"
+  local result=""
 
-echo "Necessary parameters have been set via environment variables:"
-env | sort | grep '^K3S_' | pr -t -o 4
-echo "All remaining parameters should have been previously set via /etc/rancher/k3s/config.yaml file."
-pr -t -o 4 /etc/rancher/k3s/config.yaml
-echo
+  if [[ "$node_type" == "server" ]]; then
+    server_token_params="--token-file /tmp/k3s_token_server --agent-token-file /tmp/k3s_token_agent"
+    if [[ "$server_url" == "" ]]; then
+      result="server --cluster-init ${server_token_params}"
+    else
+      result="server --server ${server_url:?} ${server_token_params:?}"
+    fi
+  elif [[ "$node_type" == "agent" ]]; then
+    agent_token_params="--token-file /tmp/k3s_token_agent"
+    if [[ "$server_url" == "" ]]; then
+      >&2 echo "server url cannot be empty for agent node type"
+      exit 1
+    fi
+    result="agent --server ${server_url:?} ${agent_token_params:?}"
+  else
+    >&2 echo "invalid node type"
+    exit 1
+  fi
 
-if [[ "${K3S_URL:=none}" == "none" && "$NODE_TYPE" == "server" ]]; then
-  echo "init server node as new cluster"
-  /bin/sh /root/k3s-install.sh -- server --cluster-init
-elif [[ "${K3S_URL:=none}" != "none" && "$NODE_TYPE" == "server" ]]; then
-  echo "init server node connecting to existing cluster: $K3S_URL"
-  /bin/sh /root/k3s-install.sh -- server
-elif [[ "${K3S_URL:=none}" != "none" && "$NODE_TYPE" == "agent" ]]; then
-  echo "init agent node connecting to existing cluster: $K3S_URL"
-  /bin/sh /root/k3s-install.sh -- agent
-else
-  echo "invalid node type and k3s url combination."
-  exit 1
-fi
+  echo $result
+}
 
-echo "adding marker file indicating the node has been initialized..."
-touch /etc/rancher/k3s/status/k3s.$NODE_TYPE.initialized
+invoke_install_script() {
+  local install_parameters="$(determine_install_params $NODE_TYPE $SERVER_URL)"
+  echo "Using commandline parameters:"
+  echo "    ${install_parameters}"
+  echo "All remaining parameters (from: /etc/rancher/k3s/config.yaml):"
+  pr -t -o 4 /etc/rancher/k3s/config.yaml
+  echo
 
-echo "Done"
+  export INSTALL_K3S_SKIP_DOWNLOAD=true
+  /bin/sh /root/k3s-install.sh -- ${install_parameters:?}
+  echo
+
+  echo "adding marker file indicating the node has been initialized..."
+  touch /etc/rancher/k3s/status/k3s.$NODE_TYPE.initialized
+  echo "Done"
+}
+
+/usr/bin/timeout --verbose 90s invoke_install_script
