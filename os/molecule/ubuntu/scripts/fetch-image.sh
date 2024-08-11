33118baea60dd3c7c241fe315559a93c99ec9534
Peter Pathirana <peter@latent.io>
2024-08-10 23:21:08 -0400

    feat(os): add support for amd64 architecture (in addition to arm64) for homelab_ops.os collection (#136)

--- os/molecule/ubuntu/scripts/fetch-image.sh
+++ os/molecule/ubuntu/scripts/fetch-image.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 set -euo pipefail
 
+url_resolved_to_random_ip() {
+  local url=$1
+  local hostname="$(echo $url | sed -E -n 's|([a-z0-9]*\:\/\/)([^\\/]+)\/.*|\2|p')"
+  local random_ip="$(dig +short $hostname | sort --random-sort | head -1)"
+  echo $url | sed -E -n 's|([a-z0-9]*\:\/\/)([^\\/]+)(\/.*)|\1'$random_ip'\3|p'
+}
+
 fetch_image() {
   local url=$1
   local output_file=$2
   local timeout=$3
+  local retries=$4
+
+  rm -f ${output_file}.xz
 
   local attempt=0
-  local retries=3
-
   while [ $attempt -lt $retries ]; do
     echo "Attempt $attempt"
-    rm -f ${output_file}.xz
+    local attempt_url="$(url_resolved_to_random_ip $url)"
+    echo "    URL: $attempt_url"
     set +e
-    /usr/bin/timeout --verbose $timeout wget -q $url -O ${output_file}.xz
+    /usr/bin/timeout --verbose $timeout wget \
+      -nv \
+      --connect-timeout=10 \
+      --continue \
+      --no-check-certificate \
+      --output-document=${output_file}.xz \
+      $attempt_url
     exit_code=$?
     set -e
     if [[ $exit_code -eq 0 ]]; then
@@ -33,9 +48,10 @@ main() {
   local url=$1
   local output_file=$2
   local timeout=$3
+  local retries=$4
 
   echo "Fetching image from $url..."
-  fetch_image $url $output_file $timeout
+  fetch_image $url $output_file $timeout $retries
 
   echo "Unpacking image..."
   xz -d ${output_file}.xz
@@ -46,6 +62,7 @@ main() {
 OUTPUT_FILE=""
 IMAGE_URL=""
 TIMEOUT="5m"
+RETRIES="3"
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -58,6 +75,9 @@ while [ $# -gt 0 ]; do
     --timeout)
       TIMEOUT="$2"; shift
       ;;
+    --retries)
+      RETRIES="$2"; shift
+      ;;
     *)
       echo "Invalid parameter: ${1}"; echo; exit 1
   esac
@@ -69,4 +89,4 @@ if [[ -z "$IMAGE_URL" || -z "$OUTPUT_FILE" ]]; then
   exit 1
 fi
 
-main ${IMAGE_URL:?} ${OUTPUT_FILE:?} ${TIMEOUT:?}
+main ${IMAGE_URL:?} ${OUTPUT_FILE:?} ${TIMEOUT:?} ${RETRIES:?} 2>&1
